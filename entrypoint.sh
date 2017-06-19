46b609af19267e481969b07646871f45ba46931a
msato <msato@altus5.com>
2017-06-19 16:51:23 +0900

    データコンテナをマウントした初回はオーナーが異なってエラーになる不具合を修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,22 +3,22 @@ set -e
 trap 'echo "エラーが発生しました。bash を起動します。" 1>&2; bash' ERR
 
 _init() {
+  ## データコンテナをマウントした初回はオーナーが異なるので chown する
+  sudo chown -R nobita:nobita /home/nobita/.npm
+  sudo chown -R nobita:nobita /srv/web/node_modules
   ## .a5cache の配置
   if [ ! -e /home/nobita/.npm/.a5 ]; then
     sudo cp -ra /home/nobita/.a5cache/.npm /home/nobita/
     sudo touch /home/nobita/.npm/.a5
-    sudo chown -R nobita:nobita /home/nobita/.npm
   fi
   if [ ! -e /srv/web/app ]; then
     sudo mkdir -p /srv
     sudo cp -ra /home/nobita/.a5cache/web /srv/
     sudo touch /srv/web/node_modules/.a5
-    sudo chown -R nobita:nobita /srv/web
   fi
   if [ ! -e /srv/web/app/node_modules/.a5 ]; then
     sudo cp -ra /home/nobita/.a5cache/web/node_modules/* /srv/web/node_modules
     sudo touch /srv/web/node_modules/.a5
-    sudo chown -R nobita:nobita /srv/web/node_modules
   fi
 }
 
